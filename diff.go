68f4bf08ec48e8da702070a420b0fe3321ccdf27
Michael Singha <michaelsingha@protonmail.com>
2021-10-02 02:13:32 +0530

    Commiting diff.go with new changes.

--- diff.go
+++ diff.go
@@ -1,38 +1,25 @@
-<<<<<<< HEAD
-package main
-
-import (
-	"fmt"
-)
-
-func diffAndsum(a int, b int) (int, int){
-	var sum = (a + b) 
-	var diff = (a - b)
-	return sum, diff
-}
- func main(){
-
-	//fmt.Println()
-	a:= 7
-	b:= 5
-
-	sum, diff:= diffAndsum(a,b)
-	
-	fmt.Println(sum)
-	fmt.Println(diff)
- }
-=======
-
 package main
 
 import "fmt"
 
-func addition(a int, b int) int{
+func substraction(a int, b int) int{
 	return a - b
 }
 
 func main(){
-	fmt.Println(addition(5,2))
-
+	fmt.Println(substraction(6,4))
 }
->>>>>>> ae241788df57e335e4e249f93750bd001501c28c
+
+/* or
+func main(){
+	a:=2
+	b:=3
+ addition(a,b)
+}
+func addition(a int, b int){
+	
+	c := a + b
+	fmt.Println("The sum is:", c)
+}
+*/
+
